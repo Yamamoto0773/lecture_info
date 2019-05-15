d8c6d2ff08ed90ed1f953a9d203d624ddab178b7
nanamin <nanamin0634@gmail.com>
2019-05-15 12:17:29 +0900

    add action
    slackに休講補講情報を送れた

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,10 +1,34 @@
+require 'open-uri'
+require 'nokogiri'
+
 class SlackController < ApplicationController
   def send_helloworld
     client = Slack::Web::Client.new
 
-    client.auth_test
     client.chat_postMessage(channel: '#develop', text: 'Hello World', as_user: true)
 
     render plain: 'hello world'
   end
+
+  def send_lecture_info
+    url = 'http://www.nagano-nct.ac.jp/current/cancel_info_5th.php'
+    doc = Nokogiri::HTML(open(url))
+
+    keys = ['休講日', '補講日', '科目名', '教室', '学科', '教員', '備考']
+
+    message = ''
+
+    doc.css('.cancel').each { |node|
+      table = Nokogiri::HTML(node.to_xhtml)
+      table.css('td').each { |cell|
+        p cell.content
+      }
+    }
+    
+    # client = Slack::Web::Client.new
+    # client.chat_postMessage(channel: '#develop', text: message, as_user: true)
+
+    render plain: message
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'slack/send_helloworld' => 'slack#send_helloworld'
+  get 'slack/send_lecture_info' => 'slack#send_lecture_info'
 
 end
