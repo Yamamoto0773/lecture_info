49086dcca471886ecfa1183f2346c0e8bd49d85b
nanamin <nanamin0634@gmail.com>
2019-05-19 18:46:39 +0900

    add/update slack mailer
    Slack用Mailerを実装

--- app/mailers/delivery_methods/slack_mail_delivery_method.rb
+++ app/mailers/delivery_methods/slack_mail_delivery_method.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'uri'
+
 class SlackMailDeliveryMethod
   attr_accessor :settings
 
@@ -6,19 +9,12 @@ class SlackMailDeliveryMethod
   end
 
   def deliver!(mail)
-    attachments = [to_attachment(mail)]
+    attachments = [JSON.parse(mail.body.to_s)]
     channel = self.settings[:channel]
-
+    
     client = Slack::Web::Client.new(token: self.settings[:api_token])
-    client.chat_postMessage(channel: channel, text: attachments, as_user: true)
+    client.chat_postMessage(channel: channel, attachments: attachments, as_user: true)
   end
 
   private
-
-  def to_attachment(mail)
-    {
-      title: mail.subject,
-      text: mail.body.to_s,
-    }
-  end
 end
--- app/mailers/lecture_mailer.rb
+++ app/mailers/lecture_mailer.rb
@@ -1,5 +1,55 @@
+require 'json'
+
 class LectureMailer < ApplicationMailer
-  def new_info(lectures)
-    mail subject: '新しい休講/補講情報'
+  def new_info(lecture)
+    @lecture_json = to_attachment_json(
+      lecture,
+      fallback: '新しい休講補講情報です',
+      color: 'good',
+      pretext: '新しい休講補講情報です',
+    )
+    mail
+  end
+
+  def update_info(lecture)
+    @lecture_json = to_attachment_json(
+      lecture,
+      fallback: '休講補講情報が更新されました',
+      color: 'warning',
+      pretext: '休講補講情報が更新されました',
+    )
+    mail
+  end
+
+  private
+
+  def to_attachment_json(lecture, options = {})
+    hash = {}
+
+    hash.merge!(options)
+
+    field_arr = []
+    field_arr << {
+      title: '科目',
+      value: lecture.subject,
+      short: false
+    }
+    field_arr << {
+      title: '休講日',
+      value: "#{lecture.canceled_on} #{(lecture.canceled_section_beg + 1)/2}-#{(lecture.canceled_section_end + 1)/2}コマ目",
+      short: true
+    }
+    field_arr << {
+      title: '補講日',
+      value: "#{lecture.supplemented_on} #{(lecture.supplemented_section_beg + 1)/2}-#{(lecture.supplemented_section_end + 1)/2}コマ目",
+      short: true
+    }
+    if lecture.remarks.present?
+      field_arr << { title: '備考', value: lecture.remarks, short: false }
+    end
+    
+    hash.store('fields', field_arr)
+
+    hash.to_json
   end
 end
