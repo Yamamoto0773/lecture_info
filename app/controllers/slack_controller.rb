79b2f19c468ab80cb44a7fd9a5012b74b64d05a2
nanamin <nanamin0634@gmail.com>
2019-05-19 13:52:07 +0900

    add action
    テスト用にhtmlファイルからスクレイピングできるアクションを追加

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,45 +1,21 @@
-require 'open-uri'
-require 'nokogiri'
-require 'nkf'
-
 class SlackController < ApplicationController
   def send_helloworld
-    client = Slack::Web::Client.new
+    ret = LectureMailer.new_info.deliver_now
+    render plain: ret
+  end
 
-    client.chat_postMessage(channel: '#develop', text: 'Hello World', as_user: true)
-
-    render plain: 'hello world'
+  def new
+    render 'slack/new'
   end
 
   def send_lecture_info
-    url = 'http://www.nagano-nct.ac.jp/current/cancel_info_5th.php'
-    doc = Nokogiri::HTML(open(url))
+    before_time = Time.zone.now
 
-    table_headers = ['休講日', '補講日', '科目名', '教室', '学科', '教員', '備考']
-    table_keys = [:canceled_on, :supplemented_on, :subject_and_class, :room, :department, :staff, :remarks]
+    lecture = Scraping::Lecture.new(file_params)
+    lecture.scrape_from_file
 
-    doc.css('.cancel').each { |node|
-      table = Nokogiri::HTML(node.to_xhtml)
-
-      # 全角英数字を半角英数字，全角スペースを半角スペース，半角カナを全角カナに変換
-      # さらに先頭と末尾の空白を除去
-      table_values = table.css('td').map { |node| 
-        NKF.nkf('-wZ1X' ,node.content).strip
-      }
-    
-      # 補講日と休講日が逆になっているテーブルが存在する
-      unless table.at_css('th').content == table_headers[0]
-        tmp = table_values[0].dup
-        table_values[0] = table_values[1]
-        table_values[1] = tmp
-      end
-
-      table_hash = Hash[*[table_keys[0...table_values.size], table_values].transpose.flatten]
-      lecture = Lecture.new(table_hash)
-      lecture.save!
-
-      break
-    }
+    p created = Lecture.where(created_at: before_time..Time.zone.now)
+    p updated = Lecture.where(updated_at: before_time..Time.zone.now)
 
     message = ''
     
@@ -47,6 +23,12 @@ class SlackController < ApplicationController
     # client.chat_postMessage(channel: '#develop', text: message, as_user: true)
 
     render plain: message
+  end
+
+  private
+
+  def file_params
+    params.permit(:file)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'slack/send_helloworld' => 'slack#send_helloworld'
-  get 'slack/send_lecture_info' => 'slack#send_lecture_info'
+  get 'slack/new' => 'slack#new'
+  post 'slack/send_lecture_info' => 'slack#send_lecture_info'
 
 end
